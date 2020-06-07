85c65bc7df8ec05fa78130d4bf75faf317d8358e
marcos-gabriel <marcoseca772@gmail.com>
2020-06-06 22:11:33 -0300

    Quinto-dia (BD)

---
+++ src/database/db.js
@@ -0,0 +1,83 @@
+// Importar a dependência do sqlite3
+const sqlite3 = require("sqlite3").verbose()
+
+// Criar o objeto que irá fazer alterações no banco de dados
+const db = new sqlite3.Database("./src/database/database.db") //Constructor ou classe
+
+module.exports = db
+// Utilizar o objeto de banco de dados para nossas operações
+db.serialize(() => {
+//     // Com comandos SQL eu vou:
+    
+    
+//     // 1- Criar uma tabela
+//     db.run(`
+//         CREATE TABLE IF NOT EXISTS places (
+//             id INTEGER PRIMARY KEY AUTOINCREMENT,
+//             image TEXT,
+//             name TEXT,
+//             address TEXT,
+//             address2 TEXT,
+//             state TEXT,
+//             city TEXT,
+//             items TEXT
+//         );
+//     `)
+
+
+//     // 2- Inserir dados na tabela
+//     const query = `
+//         INSERT INTO places (
+//             image,
+//             name,
+//             address,
+//             address2,
+//             state,
+//             city,
+//             items
+//         ) VALUES (?,?,?,?,?,?,?);
+//     `
+
+//     const values = [
+//         "https://images.unsplash.com/photo-1533626904905-cc52fd99285e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//         "Papersider",
+//         "Guilherme Gemballa, Jardim América",
+//         "Nº 260",
+//         "Santa Catarina",
+//         "Rio do Sul",
+//         "Papéis e Papelão"
+//     ]
+
+//     function afterInsertData(err) {
+//         if(err) {
+//             return console.log(err)
+//         }
+
+//         console.log("Cadastrado com sucesso")
+//         console.log(this)
+//     }
+
+//     db.run(query, values, afterInsertData)
+
+
+//     // 3- Consultar dados na tabela
+//     // db.all(`SELECT * FROM places`, function(err, rows) {
+//     //     if(err) {
+//     //         return console.log(err)
+//     //     }
+
+//     //     console.log("Aqui estão seus registros: ")
+//     //     console.log(rows)
+//     // })
+
+
+    // 4- Deletar dados da tabela
+    // db.run(`DELETE FROM places WHERE id = ?`, [3], function (err) {
+    //     if(err) {
+    //         return console.log(err)
+    //     }
+
+    //     console.log("Registro deletado com sucesso!")
+    // })
+
+})
